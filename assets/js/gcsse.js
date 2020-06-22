7e519dd050f6204fe095892f5df3f7859659ac8a
Hiroki.T <hirodora@me.com>
2020-06-23 01:50:49 +0900

    create: elm app

---
+++ assets/js/gcsse.js
@@ -0,0 +1,6 @@
+// Copyright (c) 2020 Hiroki Takemura (kekeho)
+// 
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+var app = Elm.Main.init();
---
+++ src/Main.elm
@@ -0,0 +1,72 @@
+-- Copyright (c) 2020 Hiroki Takemura (kekeho)
+-- 
+-- This software is released under the MIT License.
+-- https://opensource.org/licenses/MIT
+
+module Main exposing (..)
+
+import Html exposing (..)
+import Browser
+import Browser.Navigation as Nav
+import Url
+
+
+main : Program () RootModel Msg
+main =
+    Browser.application
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        , onUrlRequest = UrlRequested
+        , onUrlChange = UrlChanged
+    }
+
+
+type alias RootModel =
+    { key : Nav.Key
+    , url : Url.Url
+    }
+
+
+init : () -> Url.Url -> Nav.Key -> (RootModel, Cmd Msg)
+init _ url key =
+    (RootModel key url
+    , Cmd.none)
+
+
+type Msg
+    = UrlRequested Browser.UrlRequest
+    | UrlChanged Url.Url
+
+
+update : Msg -> RootModel -> (RootModel, Cmd Msg)
+update msg model =
+    case msg of
+        UrlRequested urlRequest ->
+            case urlRequest of
+                Browser.Internal url ->
+                    ( model, Nav.pushUrl model.key (Url.toString url) )
+
+                Browser.External href ->
+                    ( model, Nav.load href )
+
+        UrlChanged url ->
+            ( { model | url = url }
+            , Cmd.none
+            )
+
+
+subscriptions : RootModel -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+view : RootModel -> Browser.Document Msg
+view model =
+    { title = "GCSSEditor"
+    , body =
+        [ div []
+            [ text "GCSSEditor" ]
+      ]
+    }
